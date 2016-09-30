8a048da536231774ac19432b3c4b1332bf372cd8
Charlottha <charlotte.kruger7@gmail.com>
2016-09-30 09:43:20 +0200

    added index.php and linked to the tile-module view

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/bootstrap.css" type="text/css"/>
+        <link rel="stylesheet" href="css/site.css" type="text/css"/>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" type="text/css"/>
+        <!--todo: proper favicon-->
+        <link rel="icon" href="content/stadium-hotel-logo.jpg"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <title>The Stadium Hotel</title>
+    </head>
+    <body>
+        <header>
+            <!--contact details and logo-->
+            <nav>
+                <!--navigation-->
+            </nav>
+        </header>
+        <main>
+            <section>
+                <!--quick booking-->
+            </section>
+            <section>
+                <?php
+                    include 'views/tile-module.html';
+                ?>
+            </section>
+            <section>
+                <!--contact form-->
+            </section>
+        </main>
+        <footer>
+            <!--copyright, date and links-->
+        </footer>
+        
+        <script src="js/jquery-3.1.0.min.js">
+        </script>
+        <script src="js/bootstrap.js">
+        </script>
+    </body>
+</html>
