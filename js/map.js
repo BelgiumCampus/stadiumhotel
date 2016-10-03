185f3471329fad4e24f8648ad48b3a356a3f406a
Charlottha <charlotte.kruger7@gmail.com>
2016-10-03 10:15:47 +0200

    points-of-interest map and content

---
+++ js/map.js
@@ -0,0 +1,121 @@
+var map;
+function initMap() {
+  var stadiumHotel = {lat: -33.934627, lng: 25.603518};
+
+  map = new google.maps.Map(document.getElementById('map'), {
+      center: stadiumHotel,
+      zoom: 12
+  });
+    //the map is limited to 10 markers
+    //1. hotel
+  var hotel = new google.maps.Marker({
+      position: stadiumHotel,
+      map: map,
+      title: 'Stadium Hotel',
+      animation: google.maps.Animation.DROP
+  });
+  hotel.addListener('click', function() {
+      new google.maps.InfoWindow({
+          content: "<div><h3>The Stadium Hotel</h3><p>This is where you'll be staying.</p></div>"
+      }).open(map, hotel);
+  });
+    //2. memorial
+  var horses = new google.maps.Marker({
+      position: {lat: -33.962162, lng: 25.608809},
+      map: map,
+      title: 'Horse Memorial'
+  });
+  horses.addListener('click', function() {
+      new google.maps.InfoWindow({
+          content: "<div><h3>The Horse Memorial</h3><p>Read more about the Horse Memorial <a href='http://www.nmbt.co.za/listing/horse_memorial.html' target='_blank'>here</a>.</p></div>"
+      }).open(map, horses);
+  });
+    //3. museum
+  var museum = new google.maps.Marker({
+      position: {lat: -33.899428, lng: 25.572550},
+      map: map,
+      title: 'Red Location Museum'
+  });
+  museum.addListener('click', function() {
+      new google.maps.InfoWindow({
+          content: "<div><h3>Red Location Museum</h3><p>Read more about the Red Location Museum <a href='http://www.nmbt.co.za/listing/red_location_museum.html' target='_blank'>here</a>.</p></div>"
+      }).open(map, museum);
+  });
+    //4. reserve
+  var reserve = new google.maps.Marker({
+      position: {lat: -33.962032, lng: 25.620269},
+      map: map,
+      title: 'Donkin Reserve'
+  });
+  reserve.addListener('click', function() {
+      new google.maps.InfoWindow({
+          content: "<div><h3>Donkin Reserve</h3><p>Read more about the Donkin Reserve <a href='http://www.nmbt.co.za/listing/donkin_reserve_pyramid_and_lighthouse.html' target='_blank'>here</a>.</p></div>"
+      }).open(map, reserve);
+  });
+    //5. brewery
+  var brewery = new google.maps.Marker({
+      position: {lat: -33.965759, lng: 25.623426},
+      map: map,
+      title: 'Dockside Brewery'
+  });
+  brewery.addListener('click', function() {
+      new google.maps.InfoWindow({
+          content: "<div><h3>Dockside Brewery</h3><p>Read more about the Dockside Brewery <a href='http://www.docksidebrewery.co.za/' target='_blank'>here</a>.</p></div>"
+      }).open(map, brewery);
+  });
+    //6. water world
+  var water = new google.maps.Marker({
+      position: {lat: -33.971998, lng: 25.645317},
+      map: map,
+      title: 'Splash Water World'
+  });
+  water.addListener('click', function() {
+      new google.maps.InfoWindow({
+          content: "<div><h3>Splash Water World</h3><p>Read more about Splash Water World <a href='http://splashwaterworldpe.co.za/' target='_blank'>here</a>.</p></div>"
+      }).open(map, water);
+  });
+    //7. art museam
+  var art = new google.maps.Marker({
+      position: {lat: -33.965546, lng: 25.611263},
+      map: map,
+      title: 'Nelson Mandela Metropolitan Art Museum'
+  });
+  art.addListener('click', function() {
+      new google.maps.InfoWindow({
+          content: "<div><h3>Nelson Mandela Metropolitan Art Museum</h3><p>Read more about the Nelson Mandela Metropolitan Art Museum <a href='http://www.artmuseum.co.za/' target='_blank'>here</a>.</p></div>"
+      }).open(map, art);
+  });
+    //8. casino
+  var casino = new google.maps.Marker({
+      position: {lat: -33.982161, lng: 25.657217},
+      map: map,
+      title: 'Boardwalk Hotel and Casino'
+  });
+  casino.addListener('click', function() {
+      new google.maps.InfoWindow({
+          content: "<div><h3>Boardwalk Hotel and Casino</h3><p>Read more about the Boardwalk Hotel and Casino <a href='https://www.suninternational.com/boardwalk/' target='_blank'>here</a>.</p></div>"
+      }).open(map, casino);
+  });
+    //9. airforce museum
+  var airforce = new google.maps.Marker({
+      position: {lat: -33.989534, lng: 25.617878},
+      map: map,
+      title: 'South African Air Force Museum'
+  });
+  airforce.addListener('click', function() {
+      new google.maps.InfoWindow({
+          content: "<div><h3>South African Air Force Museum</h3><p>Read more about the South African Air Force Museum <a href='http://saafmuseum.org.za/' target='_blank'>here</a>.</p></div>"
+      }).open(map, airforce);
+  });
+    //10. penguin rescue
+  var samrec = new google.maps.Marker({
+      position: {lat: -34.013455, lng: 25.690610},
+      map: map,
+      title: 'Samrec Penguin Rescue Station'
+  });
+  samrec.addListener('click', function() {
+      new google.maps.InfoWindow({
+          content: "<div><h3>Samrec Penguin Rescue Station</h3><p>Read more about the Samrec Penguin Rescue Station <a href='http://www.samrec.org.za' target='_blank'>here</a>.</p></div>"
+      }).open(map, samrec);
+  });
+}
