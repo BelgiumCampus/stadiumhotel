af23a1e9c5aae4f2fcb5bde0a5d6d939b1092ee2
AliciaCameron14 <cameron.a@belgiumcampus.ac.za>
2016-10-04 13:20:02 +0200

    Conference booking

---
+++ bookConference.php
@@ -0,0 +1,5 @@
+<?php 
+    require('views/header.html');
+    require('views/bookConference.php');
+    require('views/footer.html');
+?>
---
+++ views/bookConference.php
@@ -0,0 +1,91 @@
+    <main>
+        <div class="container-fluid">
+            <div class="row">
+
+                <form class="form-horizontal">
+                    <fieldset>
+
+                        <!-- Form Name -->
+                        <legend class="text-center">Build your conference</legend>
+
+                        <!-- Text input-->
+                        <div class="form-group">
+                            <label class="col-md-4 control-label" for="textinput">Company Name</label>
+                            <div class="col-md-4">
+                                <input id="textinput" name="textinput" type="text" placeholder="Company name" class="form-control input-md">
+                            </div>
+                        </div>
+                        <!-- Text input-->
+                        <div class="form-group">
+                            <label class="col-md-4 control-label" for="textinput">Contact Person</label>
+                            <div class="col-md-4">
+                                <input id="textinput" name="textinput" type="text" placeholder="Contact Person" class="form-control input-md">
+                            </div>
+                        </div>
+                        <!-- Text input-->
+                        <div class="form-group">
+                            <label class="col-md-4 control-label" for="textinput">Contact Number</label>
+                            <div class="col-md-4">
+                                <input id="textinput" name="textinput" type="text" placeholder="Contact Number" class="form-control input-md">
+                            </div>
+                        </div>
+                        <!-- Text input-->
+                        <div class="form-group">
+                            <label class="col-md-4 control-label" for="textinput">Email Address</label>
+                            <div class="col-md-4">
+                                <input id="textinput" name="textinput" type="text" placeholder="Email Address" class="form-control input-md">
+                            </div>
+                        </div>
+                         <!-- Date input-->
+                        <div class="form-group">
+                            <label class="col-md-4 control-label" for="textinput">Email Address</label>
+                            <div class="col-md-4">
+                                <input id="textinput" name="textinput" type="text" placeholder="Email Address" class="form-control input-md">
+                            </div>
+                        </div>
+                        
+                        
+
+                        <!-- Multiple Radios (inline) -->
+                        <div class="form-group">
+                            <label class="col-md-4 control-label" for="radios">Inline Radios</label>
+                            <div class="col-md-4">
+                                <label class="radio-inline" for="radios-0">
+                                    <input type="radio" name="radios" id="radios-0" value="1" checked="checked"> 1
+                                </label>
+                                <label class="radio-inline" for="radios-1">
+                                    <input type="radio" name="radios" id="radios-1" value="2"> 2
+                                </label>
+                                <label class="radio-inline" for="radios-2">
+                                    <input type="radio" name="radios" id="radios-2" value="3"> 3
+                                </label>
+                                <label class="radio-inline" for="radios-3">
+                                    <input type="radio" name="radios" id="radios-3" value="4"> 4
+                                </label>
+                            </div>
+                        </div>
+
+                        <!-- Multiple Checkboxes -->
+                        <div class="form-group">
+                            <label class="col-md-4 control-label" for="checkboxes">Multiple Checkboxes</label>
+                            <div class="col-md-4">
+                                <div class="checkbox">
+                                    <label for="checkboxes-0">
+                                        <input type="checkbox" name="checkboxes" id="checkboxes-0" value="1"> Option one
+                                    </label>
+                                </div>
+                                <div class="checkbox">
+                                    <label for="checkboxes-1">
+                                        <input type="checkbox" name="checkboxes" id="checkboxes-1" value="2"> Option two
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                    </fieldset>
+                </form>
+
+
+            </div>
+        </div>
+    </main>
