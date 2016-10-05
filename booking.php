a288bfc8ef8ad714065444a38bae3a9a2289208f
Charlottha <charlotte.kruger7@gmail.com>
2016-10-05 15:55:13 +0200

    Booking root page

---
+++ booking.php
@@ -0,0 +1,11 @@
+<?php
+    include_once('views/header.html');
+?>
+Imagine buttons here <br/>
+
+Imagine content here after clicking one of the buttons
+<!--include here-->
+
+<?php
+    include_once('views/footer.html');
+?>
