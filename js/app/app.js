ec164bba65b7c7c6916730e2efccdfd049f5f349
Charlottha <charlotte.kruger7@gmail.com>
2016-10-05 15:09:14 +0200

    Active page indicator

--- js/app/app.js
+++ js/app/app.js
@@ -1,7 +1,14 @@
 (function () {
     $('document').ready(function () {
+        
+        var url = window.location.pathname.split('/');
+        var currentPage = url[url.length-1];
+        $('[href="' + currentPage + '"]').addClass('active-page');
+        
         $('body').on('click', '.mobile-toggle a', function () {
             $('nav ul').toggleClass('show-menu hide-menu');
+            
+            
         })
 
         $(function () {
