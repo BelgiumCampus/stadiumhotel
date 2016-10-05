b0cd0e4fd469d90b53f433b1946de03398c5c504
Charlottha <charlotte.kruger7@gmail.com>
2016-10-05 13:27:40 +0200

    Tucked points of interest

--- views/poi.php
+++ views/poi.php
@@ -7,6 +7,7 @@
                   <p>
                       The Stadium Hotel is conveniently close to various exciting tourist attractions and significant locations. <br/> The map provides you with a bird's eye view of 10 points of interest within a 10 kilometer radius of the hotel. <br/> Click away!
                   </p>
+                  <hr/>
               </section>
           </div>
       </div>
@@ -18,67 +19,68 @@
               </div>
               <div id="poi" class="col-lg-6 col-md-6">
                   <section id="main-points">
-                      <h3>Main Points of Interest</h3>
+                      <h3 class="sliding-banner">Main Points of Interest</h3>
                       <div class="row">
-                          <div class="col-lg-12 sliding-banner tile-color-3">
+                          <a href="http://www.nmbstadium.com/" target="_blank" class="sliding-banner col-lg-9 col-sm-12 col-xs-12 tile-color-3">
                               <p>
-                                  1km (12 minute walk) from Nelson Mandela Bay Stadium. <a href="http://www.nmbstadium.com/" target="_blank"><i class="fa fa-arrow-right"></i></a>  
+                                  1km (12 minute walk) from Nelson Mandela Bay Stadium <i class="fa fa-arrow-right"></i>  
                               </p>
-                          </div>
+                          </a>
                       </div>
                       <div class="row">
-                          <div class="col-lg-10 sliding-banner tile-color-1">
+                          <a href="http://stgeorgespark.nmmu.ac.za/" target="_blank" class="col-lg-9 col-sm-12 col-xs-12 sliding-banner tile-color-1">
                               <p>
-                                  5km from St. George's Park. <a href="http://stgeorgespark.nmmu.ac.za/" target="_blank"><i class="fa fa-arrow-right"></i></a> 
+                                  5km from St. George's Park <i class="fa fa-arrow-right"></i> 
                               </p>
-                          </div>
+                          </a>
                       </div>
                       <div class="row">
-                          <div class="col-lg-9 sliding-banner tile-color-2">
+                          <a href="http://www.bayworld.co.za/" target="_blank" class="col-lg-9 col-sm-12 col-xs-12 sliding-banner tile-color-2">
                               <p>
-                                  9km from Bayworld Museum, Oceanarium &amp; Snake Park. <a href="http://www.bayworld.co.za/" target="_blank"><i class="fa fa-arrow-right"></i></a>
+                                  9km from Bayworld Museum, Oceanarium &amp; Snake Park <i class="fa fa-arrow-right"></i>
                               </p> 
-                          </div>
+                          </a>
                       </div>
                   </section>
                   <section id="other-points">
-                      <h3>Other Points of Interest</h3>
+                      <h3 class="sliding-banner">Other Points of Interest</h3>
                       <div class="row">
-                          <div class="col-lg-11 sliding-banner tile-color-1">
+                          <a href="http://saafmuseum.org.za/" target="_blank" class="col-lg-9 col-sm-12 col-xs-12 sliding-banner tile-color-1">
                               <p>
-                                  13km from the South African Air Force Museum. <a href="http://saafmuseum.org.za/" target="_blank"><i class="fa fa-arrow-right"></i></a> 
+                                  13km from the South African Air Force Museum <i class="fa fa-arrow-right"></i> 
                               </p>
-                          </div>
+                          </a>
                       </div>
                       <div class="row">
-                          <div class="col-lg-10 sliding-banner tile-color-3">
+                          <a href="http://www.samrec.org.za" target="_blank" class="col-lg-9 col-sm-12 col-xs-12 sliding-banner tile-color-3">
                               <p>
-                                  15km from the Samrec Penguin Rescue Station. <a href="http://www.samrec.org.za" target="_blank"><i class="fa fa-arrow-right"></i></a> 
+                                  15km from the Samrec Penguin Rescue Station <i class="fa fa-arrow-right"></i> 
                               </p>
-                          </div>
+                          </a>
                       </div>
                       <div class="row">
-                          <div class="col-lg-12 sliding-banner tile-color-2">
+                          <a href="http://www.seaviewpredatorpark.com" target="_blank" class="col-lg-9 col-sm-12 col-xs-12 sliding-banner tile-color-2">
                               <p>
-                                  23km from Seaview Predator Park. <a href="http://www.seaviewpredatorpark.com" target="_blank"><i class="fa fa-arrow-right"></i></a>
+                                  23km from Seaview Predator Park <i class="fa fa-arrow-right"></i>
                               </p> 
-                          </div>
+                          </a>
                       </div>
                       <div class="row">
-                          <div class="col-lg-9 sliding-banner tile-color-2">
+                          <a href="http://www.kraggakamma.co.za/" target="_blank" class="col-lg-9 col-sm-12 col-xs-12 sliding-banner tile-color-2">
                               <p>
-                                  20km from	Kragga Kamma Game Park. <a href="http://www.kraggakamma.co.za/" target="_blank"><i class="fa fa-arrow-right"></i></a>
+                                  20km from	Kragga Kamma Game Park <i class="fa fa-arrow-right"></i>
                               </p> 
-                          </div>
+                          </a>
                       </div>
                       <div class="row">
-                          <div class="col-lg-11 sliding-banner tile-color-1">
+                          <a href="http://www.autopavilion.co.za/home.htm" target="_blank" class="col-lg-9 col-sm-12 col-xs-12 sliding-banner tile-color-1">
                               <p>
-                                  26km from	Auto Pavilion. <a href="http://www.autopavilion.co.za/home.htm" target="_blank"><i class="fa fa-arrow-right"></i></a>  
+                                  26km from	Auto Pavilion <i class="fa fa-arrow-right"></i> 
                               </p>
-                          </div>
+                          </a> 
                       </div>                              
                   </section>
+                  <hr/>
               </div>
           </div>
       </div>
