6c95349b32a0108f07dc9bf7adf359db9a2eb16b
AliciaCameron14 <cameron.a@belgiumcampus.ac.za>
2016-10-05 15:53:06 +0200

    Conference booking page

---
+++ views/book-conference.php
@@ -0,0 +1,184 @@
+<main>
+    <div class="padding-bottom-lg row">
+
+        <form class="form-horizontal">
+            <fieldset class="col-md-8 col-md-offset-2 booking-form">
+
+                <!-- Form Name -->
+                <h3 class="padding-top-bottom text-center">Build your conference</h3>
+                <div class="padding-top-bottom col-md-6">
+                    <!-- Text input-->
+                    <div class="form-group">
+                        <label class="col-md-4 control-label" for="textinput">Company Name</label>
+                        <div class="col-md-7">
+                            <input id="textinput" name="textinput" type="text" placeholder="Company name" class="form-control input-md">
+                        </div>
+                    </div>
+                    <!-- Text input-->
+                    <div class="form-group">
+                        <label class="col-md-4 control-label" for="textinput">Contact Person</label>
+                        <div class="col-md-7">
+                            <input id="textinput" name="textinput" type="text" placeholder="Contact Person" class="form-control input-md">
+                        </div>
+                    </div>
+                    <!-- Text input-->
+                    <div class="form-group">
+                        <label class="col-md-4 control-label" for="textinput">Contact Number</label>
+                        <div class="col-md-7">
+                            <input id="textinput" name="textinput" type="text" placeholder="Contact Number" class="form-control input-md">
+                        </div>
+                    </div>
+                    <!-- Text input-->
+                    <div class="form-group">
+                        <label class="col-md-4 control-label" for="textinput">Email Address</label>
+                        <div class="col-md-7">
+                            <input id="textinput" name="textinput" type="text" placeholder="Email Address" class="form-control input-md">
+                        </div>
+                    </div>
+                    <!-- Date input-->
+                    <div class="form-group">
+                        <label class="col-md-4 control-label" for="appendedcheckbox">Date</label>
+                        <div class="col-md-7">
+                            <div class="input-group">
+                                <input id="appendedcheckbox" name="appendedcheckbox" class="form-control" type="text" placeholder="Conference Date">
+                                <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="padding-top-bottom col-md-6">
+
+                    <!-- Multiple Radios (inline) -->
+                    <div class="form-group">
+                        <label class="col-md-4 control-label" for="radios">Preferred Conference Room & Layout:</label>
+                        <div class="padding-bottom col-md-8">
+                            <label class="padding-right" for="radios-0">
+                                <input type="radio" name="radios" id="radios-0" value="1" checked="checked"> Kings Room Cinema Style (60 People)
+                            </label>
+                            <label class="padding-right" for="radios-1">
+                                <input type="radio" name="radios" id="radios-1" value="2"> Kings School Room (30 People)
+                            </label>
+                            <label class="padding-right" for="radios-2">
+                                <input type="radio" name="radios" id="radios-2" value="3"> Kings Board Room (20 People)
+                            </label>
+                        </div>
+                        <div class="padding-bottom col-md-8 col-md-offset-4">
+                            <label class="padding-right" for="radios-0">
+                                <input type="radio" name="radios" id="radios-0" value="1" checked="checked"> Bay Room Cinema Style (30 People)
+                            </label>
+                            <label class="padding-right" for="radios-1">
+                                <input type="radio" name="radios" id="radios-1" value="2"> Bay School Room (20 People)
+                            </label>
+                            <label class="padding-right" for="radios-2">
+                                <input type="radio" name="radios" id="radios-2" value="3"> Bay Board Room (15 People)
+                            </label>
+                        </div>
+                        <div class="padding-bottom col-md-8 col-md-offset-4">
+                            <label class="padding-right" for="radios-0">
+                                <input type="radio" name="radios" id="radios-0" value="1" checked="checked"> Warriors Board Room (15 People)
+                            </label>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-12">
+                    <!-- Multiple Checkboxes -->
+                    <div class="form-group">
+                        <label class="no-padding col-md-2 control-label" for="checkboxes">Cuisine Per Person</label>
+                        <div class="col-md-12"><br></div>
+                        
+
+                        <div class="col-md-4">
+                            <label class="col-md-4 control-label" for="checkboxes">Breakfast</label>
+                            <div class="col-md-12">
+                                <div class="checkbox">
+                                    <label for="checkboxes-1">
+                                        <input type="checkbox" name="checkboxes" id="checkboxes-2" value="2"> R15 - Tea and/or coffee
+                                    </label>
+                                </div>
+                                <div class="checkbox">
+                                    <label for="checkboxes-1">
+                                        <input type="checkbox" name="checkboxes" id="checkboxes-3" value="2"> R20 - Biscuits
+                                    </label>
+                                </div>
+                                <div class="checkbox">
+                                    <label for="checkboxes-0">
+                                        <input type="checkbox" name="checkboxes" id="checkboxes-0" value="1"> R50 - Plated
+                                    </label>
+                                </div>
+                                <div class="checkbox">
+                                    <label for="checkboxes-1">
+                                        <input type="checkbox" name="checkboxes" id="checkboxes-1" value="2"> R85 - Buffet
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4">
+                            <label class="col-md-4 control-label" for="checkboxes1">Lunch</label>
+                            <div class="col-md-12">
+                                <div class="checkbox">
+                                    <label for="checkboxes-1">
+                                        <input type="checkbox" name="checkboxes1" id="checkboxes-2" value="2"> R15 - Tea and/or coffee
+                                    </label>
+                                </div>
+                                <div class="checkbox">
+                                    <label for="checkboxes-1">
+                                        <input type="checkbox" name="checkboxes1" id="checkboxes-3" value="2"> R20 - Biscuits
+                                    </label>
+                                </div>
+                                <div class="checkbox">
+                                    <label for="checkboxes-1">
+                                        <input type="checkbox" name="checkboxes1" id="checkboxes-2" value="2"> R35 - Sandwiches
+                                    </label>
+                                </div>
+                                <div class="checkbox">
+                                    <label for="checkboxes-1">
+                                        <input type="checkbox" name="checkboxes1" id="checkboxes-3" value="2"> R45 - Pastries
+                                    </label>
+                                </div>
+                                <div class="checkbox">
+                                    <label for="checkboxes-0">
+                                        <input type="checkbox" name="checkboxes1" id="checkboxes-0" value="1"> R75 - Lunch
+                                    </label>
+                                </div>
+                                <div class="checkbox">
+                                    <label for="checkboxes-1">
+                                        <input type="checkbox" name="checkboxes1" id="checkboxes-1" value="2"> R100 - Lunch
+                                    </label>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+
+                            <label class="col-md-4 control-label" for="checkboxes1">Dinner</label>
+                            <div class="col-md-12">
+
+                                <div class="checkbox">
+                                    <label for="checkboxes-1">
+                                        <input type="checkbox" name="checkboxes1" id="checkboxes-1" value="2"> R100 - Dinner
+                                    </label>
+                                </div>
+                                <div class="checkbox">
+                                    <label for="checkboxes-1">
+                                        <input type="checkbox" name="checkboxes1" id="checkboxes-1" value="2"> R140 - Dinner
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-8 col-md-offset-2 text-center padding-top-bottom">
+                    <input class="padding-left-right no-border-radius btn-lg btn btn-default" type="submit" value="Submit">
+
+                </div>
+
+            </fieldset>
+        </form>
+
+
+    </div>
+</main>
