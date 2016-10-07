2b3bccfbf5600b50f2e4c34d46b83a69b995c936
HermenegildoMI <isaias.h@belgiumcampus.ac.za>
2016-10-07 11:05:09 +0200

    double fire event bug resolved!

--- js/booking.js
+++ js/booking.js
@@ -1,12 +1,18 @@
+(function () {
+    $(document).ready(function () {
+        $('.room-tile').hover(function () {
+            var $this = $(this);
+            console.log($this.find('.descr'));
+            $this.find('.price').toggle();
+            $this.find('.descr').toggle();
+            $this.css("background-color", "#FDC304").css("color", "#181735");
+        }, function () {
+            var $this = $(this);
+            console.log(this);
+            $this.find('.price').toggle();
+            $this.find('.descr').toggle();
+            $this.css("background-color", "rgba(0, 125, 95, .8)").css("color", "#FFF");
+        });
 
-    $('.room-tile').hover(function(){
-        var $this = $(this);
-        $this.find('.price').toggle();
-        $this.find('.descr').toggle();
-        $this.css("background-color", "#FDC304").css("color","#181735");
-    }, function(){
-        var $this = $(this);
-        $this.find('.price').toggle();
-        $this.find('.descr').toggle();
-        $this.css("background-color", "rgba(0, 125, 95, .8)").css("color","#FFF");        
-    });
+    })
+})();
