087e8fc1a87a2243308bd2847d309c890f8a9f67
HermenegildoMI <isaias.h@belgiumcampus.ac.za>
2016-10-10 15:21:16 +0200

    urlCleaner removed. standard room automated

--- js/app/app.js
+++ js/app/app.js
@@ -1,36 +1,7 @@
 (function () {
-    var urlCleaner = function (url) {
-        var mappedObjs = [];
-        var multipleParam = url.indexOf('&') > -1;
-        if (multipleParam) {
-            var holder = url.split('&');
-            holder.forEach(function (item, i) {
-                var temp = item.split('=');
-                mappedObjs.push({
-                    name: temp[0],
-                    value: temp[1]
-                });
-            });
-        } else {
-            var temp = url.split('=');
-            mappedObjs.push({
-                name: temp[0],
-                value: temp[1]
-            });
-        }
-
-        return mappedObjs;
-    }
     $('document').ready(function () {
         $('[data-toggle="tooltip"]').tooltip();
         var url = window.location.pathname.split('/');
-        var urlHref = window.location.href;
-        var containsParam = urlHref.indexOf('?') > -1;
-        var param = urlHref.substr(urlHref.indexOf('?') + 1);
-        var mappedObjs = containsParam ? urlCleaner(param) : [];
-        mappedObjs.forEach(function (item, i) {
-            $('#' + item.name).val(item.value);
-        })
 
         var currentPage = url[url.length - 1];
         if (currentPage.lastIndexOf('.php') < 0) currentPage = 'index.php';
@@ -42,14 +13,14 @@
             $('nav ul').toggleClass('show-menu hide-menu');
         })
 
-        $('body').on('change', '.room-type', function(el) {
+        $('body').on('change', '.room-type', function (el) {
             var currentVal = 0;
-            $('.room-type').each(function(i,e) {
+            $('.room-type').each(function (i, e) {
                 currentVal += $(e).val() * 2;
             })
             $('#people').val(currentVal);
         })
-        
+
         /*Datepickers*/
         $(function () {
             $('#conferencedatepicker').datepicker({
